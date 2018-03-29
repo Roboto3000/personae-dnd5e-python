174f30443d2ce5ae308db32c86694f9bfca7050a
Marcus <mtaylor3121@gmail.com>
2018-03-29 07:49:17 -0400

    code cleanup

--- personae_standalone.py
+++ personae_standalone.py
@@ -21,43 +21,6 @@ def get_modifier(score):
     """
     return (score - 10)/2
 
-def has_requirements(feat, _class, a_prof, w_prof, scores):
-    """Checks if scores, a_prof, w_prof has requirements for feat.
-    
-    Args:
-        feat (string): Feat to check requirements for.
-        _class (string): Class to check requirements for.
-        a_prof (list): List of armor proficiencies.
-        w_prof (list): List of weapon proficiencies.
-        scores (dict): Dictionary of ability scores.
-        
-    Returns:
-        True if requirements met; False otherwise.
-    """
-    if feat in ('Elemental Adept', 'Spell Sniper', 'War Caster'):
-        if not is_caster(_class):
-            return False
-    require = get_requirements(feat)
-    if require['Class'] is not '-':
-        if _class not in require['Class'].split(','):
-            return False
-    if require['Proficiency'] is not '-':
-        if require['Proficiency'] not in a_prof or w_prof:
-            return False
-    if scores['Strength']['Score'] < require['Strength']:
-        return False
-    if scores['Dexterity']['Score'] < require['Dexterity']:
-        return False
-    if scores['Constitution']['Score'] < require['Constitution']:
-        return False
-    if scores['Intelligence']['Score'] < require['Intelligence']:
-        return False
-    if scores['Wisdom']['Score'] < require['Wisdom']:
-        return False
-    if scores['Charisma']['Score'] < require['Charisma']:
-        return False
-    return True
-
 def is_caster(_class, level=1):
     """Returns True if _class is spellcaster; False if not.
     
